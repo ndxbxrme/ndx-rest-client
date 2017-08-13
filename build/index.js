0061179623c94ddf2aa46f0db49d1da6583c049a
ndxbxrme <lewis_the_cat@hotmail.com>
2017-08-13 16:20:29 +0100

    throttle #3

--- build/index.js
+++ build/index.js
@@ -12,7 +12,7 @@
   }
 
   module.factory('rest', function($http, $injector, $timeout) {
-    var auth, autoId, callRefreshFns, cloneSpecialProps, dereg, destroy, endpoints, listTransform, ndxCheck, needsRefresh, okToLoad, refreshFns, restore, restoreSpecialProps, root, socket, throttle, waiting;
+    var auth, autoId, callRefreshFns, cloneSpecialProps, dereg, destroy, endpoints, listTransform, ndxCheck, needsRefresh, okToLoad, refreshFns, restore, restoreSpecialProps, root, socket, waiting;
     okToLoad = false;
     endpoints = {};
     autoId = '_id';
@@ -26,49 +26,6 @@
       page: true,
       pageSize: true,
       error: true
-    };
-    throttle = function(func, wait, options) {
-      var args, context, later, previous, result, timeout;
-      context = void 0;
-      args = void 0;
-      result = void 0;
-      timeout = null;
-      previous = 0;
-      if (!options) {
-        options = {};
-      }
-      later = function() {
-        previous = options.leading === false ? 0 : Date.now();
-        timeout = null;
-        result = func.apply(context, args);
-        if (!timeout) {
-          context = args = null;
-        }
-      };
-      return function() {
-        var now, remaining;
-        now = Date.now();
-        if (!previous && options.leading === false) {
-          previous = now;
-        }
-        remaining = wait - (now - previous);
-        context = this;
-        args = arguments;
-        if (remaining <= 0 || remaining > wait) {
-          if (timeout) {
-            clearTimeout(timeout);
-            timeout = null;
-          }
-          previous = now;
-          result = func.apply(context, args);
-          if (!timeout) {
-            context = args = null;
-          }
-        } else if (!timeout && options.trailing !== false) {
-          timeout = setTimeout(later, remaining);
-        }
-        return result;
-      };
     };
     callRefreshFns = function() {
       var fn, i, key, len, results;
@@ -333,7 +290,50 @@
       destroy: destroy
     };
   }).run(function($rootScope, $http, rest) {
-    var root;
+    var root, throttle;
+    throttle = function(func, wait, options) {
+      var args, context, later, previous, result, timeout;
+      context = void 0;
+      args = void 0;
+      result = void 0;
+      timeout = null;
+      previous = 0;
+      if (!options) {
+        options = {};
+      }
+      later = function() {
+        previous = options.leading === false ? 0 : Date.now();
+        timeout = null;
+        result = func.apply(context, args);
+        if (!timeout) {
+          context = args = null;
+        }
+      };
+      return function() {
+        var now, remaining;
+        now = Date.now();
+        if (!previous && options.leading === false) {
+          previous = now;
+        }
+        remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0 || remaining > wait) {
+          if (timeout) {
+            clearTimeout(timeout);
+            timeout = null;
+          }
+          previous = now;
+          result = func.apply(context, args);
+          if (!timeout) {
+            context = args = null;
+          }
+        } else if (!timeout && options.trailing !== false) {
+          timeout = setTimeout(later, remaining);
+        }
+        return result;
+      };
+    };
     root = Object.getPrototypeOf($rootScope);
     root.list = function(endpoint, args, cb) {
       var RefreshFn, dereg, obj, throttledSearch;
